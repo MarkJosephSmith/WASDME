1dcfe9604b5646b7f226999684172fcd90d82f8e
u0164776 <mark.j.smith@utah.edu>
2016-10-15 15:15:41 -0600

    Better box
    made a better box, should now be able to add trigger plates

---
+++ WASDME/Assets/MarkAssets/scripts/PickupObject1.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+
+    GameObject pickedUp;
+    float pickedUpSize;
+	
+    GameObject GetMouseHoverObject(float range)
+    {
+        Vector3 possition = gameObject.transform.position;
+        RaycastHit rayHit;
+        Vector3 target = possition + Camera.main.transform.forward * range;
+        if (Physics.Linecast(possition, target, out rayHit))
+        {
+            return rayHit.collider.gameObject;
+        }
+        return null;
+    }
+
+
+    void pickUp(GameObject tryPickup)
+    {
+        if (tryPickup == null || (tryPickup.tag != "stickable"))
+        {
+            return;
+        }
+
+        pickedUp = tryPickup;
+        pickedUpSize = pickedUp.GetComponent<Renderer>().bounds.size.magnitude;
+
+
+    }
+
+    void dropObject()
+    {
+        if (pickedUp == null)
+        {
+            return;
+        }
+
+        if (pickedUp.GetComponent<Rigidbody>() != null)
+        {
+            pickedUp.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+
+        pickedUp = null;
+        pickedUpSize = 0;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (pickedUp == null)
+            {
+                pickUp(GetMouseHoverObject(5));
+            }
+            else
+            {
+                dropObject();
+            }
+
+
+
+        }
+
+        if (pickedUp != null)
+        {
+            Vector3 newPossition = gameObject.transform.position + Camera.main.transform.forward * pickedUpSize;
+            pickedUp.transform.position = newPossition;
+        }
+
+    }
+}
---
+++ WASDME/Assets/MarkAssets/scripts/PickupObject2.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject2 : MonoBehaviour
+{
+
+    GameObject mainCamera;
+    bool isCarrying;
+    GameObject carriedObject;
+    public float distance;
+    public float smooth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isCarrying)
+        {
+            carry(carriedObject);
+            checkDrop();
+        }
+        else
+        {
+            pickup();
+        }
+
+	}
+
+    void carry(GameObject o)
+    {
+        o.transform.position = Vector3.Lerp( o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+        o.transform.rotation = Quaternion.identity;
+
+    }
+
+    void pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray shootRay = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(shootRay, out rayHit))
+            {
+                liftable p = rayHit.collider.GetComponent<liftable>();
+                if (p != null)
+                {
+                    isCarrying = true;
+                    carriedObject = p.gameObject;
+                    //p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+                    p.gameObject.GetComponent<Rigidbody>().useGravity = false;
+
+                }
+            }
+        }
+    }
+
+    void checkDrop()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            dropObject();
+        }
+    }
+
+    void dropObject()
+    {
+        isCarrying = false;
+        //carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject.GetComponent<Rigidbody>().useGravity = true;
+        carriedObject = null;
+
+    }
+}
